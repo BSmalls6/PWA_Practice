60e5d25af79ecd1079d4d5024669f24da1fd4807
BSmalls6 <smallwoodbrennan@gmail.com>
2020-09-24 14:58:52 -0700

    api call

--- pwa_practice/src/App.js
+++ pwa_practice/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [data, setData] = useState([])
+
+    useEffect (()=> {
+        const fetchData = async() =>{
+            const result = await fetch("http://orangevalleycaa.org/api/videos").then(response => response.json());
+    
+    setData(result);
+};
+    fetchData();
+
+    });
+    return ( 
+    
+    <div className = "App" >
+        <header>
+            <h1>Videos</h1> 
+        </header>
     </div>
-  );
+    );
 }
 
 export default App;
